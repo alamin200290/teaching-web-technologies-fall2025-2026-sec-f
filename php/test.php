594b9a9bedb546c2e18ea0dee51b9385d1e6dcb4
alamin200290 <amin.se.aiub@gmail.com>
2025-12-03 14:46:34 +0600

    intro PHP

---
+++ php/test.php
@@ -0,0 +1,49 @@
+
+
+<?php 
+
+    $name = "";
+    $id = 12;
+    $cgpa = 3.5;
+
+    //$std = array(1, 'alamin', 3.5);
+    // $std[1];
+    // $std = [1, 'alamin', 3.5];
+    // $stds = [
+    //     [1, 'alamin', 1.5],
+    //     [3, 'alamin', 2.5],
+    //     [14, 'alamin', 3.5]
+    // ];
+
+    // $stds[2][2];
+    // $std = ['id'=>1, 'name'=>'alamin', 'cgpa'=3.5];
+    // $std['name'];
+    // $stds = [
+    //     's1'=>['id'=>1, 'name'=>'alamin', 'cgpa'=2.5],
+    //     's2'=>['id'=>2, 'name'=>'xyz', 'cgpa'=1.5],
+    //     's3'=>['id'=>3, 'name'=>'abc', 'cgpa'=3.5]
+    // ]
+    // $stds['s3']['name'];
+
+    // function sum($a,$b){
+    //     return $a+$b;
+    // }
+    // sum();
+
+    for($i=0;$i<10;$i++){
+        echo "<h1> alamin </h1>";
+    }
+
+    // if(){
+
+    // }else{
+
+    // }
+
+    //echo "test {$name} abc";
+    //print('test');
+    //$obj->
+
+?>
+
+<h2 style="color: red" onclick="alert('test')"><?php if($name !==""){echo $name;}else{echo "null";}  ?> </h2>
